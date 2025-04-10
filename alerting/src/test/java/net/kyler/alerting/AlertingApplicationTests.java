dd5435cc087ed8e1308c6bdffe52b226fe75e420
Kyler Love <kyler.love@gmail.com>
2025-04-09 22:39:21 -0400

    init

---
+++ alerting/src/test/java/net/kyler/alerting/AlertingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kyler.alerting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
